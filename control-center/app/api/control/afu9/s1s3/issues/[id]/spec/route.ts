9b782013a948b4864ff9172773ee01d2ebae4903
Andy <adaefler@me.com>
2026-02-05 10:29:34 +0100

    fix(control): resolve spec route import

--- control-center/app/api/control/afu9/s1s3/issues/[id]/spec/route.ts
+++ control-center/app/api/control/afu9/s1s3/issues/[id]/spec/route.ts
@@ -1,1 +1,1 @@
-export { POST, dynamic, revalidate } from "@/app/api/afu9/s1s3/issues/[id]/spec/route";
+export { POST, dynamic, revalidate } from "../../../../../../afu9/s1s3/issues/[id]/spec/route";
